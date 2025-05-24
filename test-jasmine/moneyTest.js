348049f2838add0b34d67be67c11d0f9f498cb43
Sriram <sriramkudupudi29@gmail.com>
2025-05-24 09:30:01 +0530

    initial testing useing the jasmine

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,12 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+describe("test suit:formatCurrency",()=>{
+    //for testing
+    it('convert ruppes to dollers:',()=>{
+        //alternative to if condition
+        expect(formatCurrency(830).toEqual('83.00'));
+    });
+    it('convert ruppes to dollers:',()=>{
+        //alternative to if condition
+        expect(formatCurrency(0).toEqual('0.00'));
+    });
+})
