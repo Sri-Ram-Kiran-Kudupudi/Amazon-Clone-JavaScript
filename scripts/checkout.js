2f925fc8db2447213e9562eaa2046eee1d187eef
Sriram <sriramkudupudi29@gmail.com>
2025-05-24 21:27:25 +0530

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import {renderCheckoutHeader} from './checkout/checkoutHeader.js'
 import {renderOrderSummary} from './checkout/orderSymmary.js'
 import {renderPaymentSummary} from './checkout/paymentSymmary.js'
 // import '../data/cart-calss.js'
+import '../data/backend-practice.js'
 renderOrderSummary();
 renderPaymentSummary();
 renderCheckoutHeader()
